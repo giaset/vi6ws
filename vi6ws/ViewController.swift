d47813ffc5d5aa746da92d97a8e06e3616bb1dd1
Gianni Settino <g@miltonandparc.com>
2014-12-14 22:33:46 -0500

    Initial Commit

---
+++ vi6ws/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vi6ws
+//
+//  Created by Gianni Settino on 2014-12-14.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
