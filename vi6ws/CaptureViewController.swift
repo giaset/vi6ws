f5789ff43feab8331e9ba740372254720cc90805
Gianni Settino <g@miltonandparc.com>
2014-12-14 23:42:15 -0500

    Add Capture View to view controller

--- vi6ws/CaptureViewController.swift
+++ vi6ws/CaptureViewController.swift
@@ -11,19 +11,28 @@ import UIKit
 class CaptureViewController: UIViewController {
 
     var captureControlsView: CaptureControlsView?
+    var captureView: CaptureView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         captureControlsView = CaptureControlsView(frame: view.frame)
         view.addSubview(captureControlsView!)
+        
+        captureView = CaptureView(frame: CGRect(x: 0, y: 44, width: view.frame.size.width, height: view.frame.size.width))
+        view.addSubview(captureView!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        UIApplication.sharedApplication().statusBarHidden = true
+    }
+    
 }
 
