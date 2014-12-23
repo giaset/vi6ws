102113f1d04476031fa199797fcde0a92dfbe186
Gianni Settino <g@miltonandparc.com>
2014-12-23 10:41:29 -0500

    Bridging header solves everything!

---
+++ vi6ws/vi6ws-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  vi6ws-Bridging-Header.h
+//  vi6ws
+//
+//  Created by Gianni Settino on 2014-12-23.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+#import "ECSlidingViewController.h"
