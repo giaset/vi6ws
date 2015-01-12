68b0461be930413876347d5d0081c3184d9a1130
Gianni Settino <g@miltonandparc.com>
2015-01-12 00:22:27 -0500

    Progress on export view

--- vi6ws/ExportView.swift
+++ vi6ws/ExportView.swift
@@ -25,10 +25,30 @@ class ExportView: UIView {
     let separator3 = UIView()
     
     let separatorHeight: CGFloat = 2
-    let buttonHeight: CGFloat = 50
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        clipsToBounds = true
+        
+        backgroundView.image = UIImage(named: "marble")
+        backgroundView.contentMode = .ScaleAspectFill
+        addSubview(backgroundView)
+        
+        closeButtonDimView.backgroundColor = UIColor(red: 0.055, green: 0.063, blue: 0.067, alpha: 1) /*#0e1011*/
+        closeButtonDimView.alpha = 0.3
+        addSubview(closeButtonDimView)
+        
+        let separatorColor = UIColor(red: 0.129, green: 0.154, blue: 0.169, alpha: 1) /*#21272b*/
+        
+        separator1.backgroundColor = separatorColor
+        addSubview(separator1)
+        
+        separator2.backgroundColor = separatorColor
+        addSubview(separator2)
+        
+        separator3.backgroundColor = separatorColor
+        addSubview(separator3)
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -37,6 +57,16 @@ class ExportView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        backgroundView.frame = bounds
+        
+        let buttonHeight = (frame.height - (3*separatorHeight))/5
+        
+        closeButtonDimView.frame = CGRect(x: 0, y: 0, width: frame.width, height: buttonHeight)
+        
+        separator1.frame = CGRect(x: 0, y: 2*buttonHeight, width: frame.width, height: separatorHeight)
+        separator2.frame = CGRect(x: 0, y: 3*buttonHeight+separatorHeight, width: frame.width, height: separatorHeight)
+        separator3.frame = CGRect(x: 0, y: 4*buttonHeight+(2*separatorHeight), width: frame.width, height: separatorHeight)
     }
     
 }
--- vi6ws/WatermarkViewController.swift
+++ vi6ws/WatermarkViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class WatermarkViewController: UIViewController {
     
     var watermarkView: WatermarkView?
+    var exportView: ExportView?
     
     var image: UIImage
     
@@ -30,6 +31,10 @@ class WatermarkViewController: UIViewController {
         watermarkView!.imageView.image = image
         view.addSubview(watermarkView!)
         
+        let exportViewHeight = view.frame.height-44-view.frame.width
+        exportView = ExportView(frame: CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: exportViewHeight))
+        view.addSubview(exportView!)
+        
         watermarkView!.backButton.addTarget(self, action: "backButtonPressed", forControlEvents: .TouchUpInside)
         watermarkView!.exportButton.addTarget(self, action: "exportButtonPressed", forControlEvents: .TouchUpInside)
     }
@@ -39,14 +44,22 @@ class WatermarkViewController: UIViewController {
     }
     
     func exportButtonPressed() {
-        SVProgressHUD.show()
+        UIView.animateWithDuration(0.3, animations: {
+            self.exportView!.frame.origin.y = self.view.frame.height-self.exportView!.frame.height
+            self.watermarkView!.backButton.enabled = false
+            self.watermarkView!.backButton.alpha = 0.3
+            self.watermarkView!.exportButton.enabled = false
+            self.watermarkView!.exportButton.alpha = 0.3
+        })
+        
+        /*SVProgressHUD.show()
         
         UIGraphicsBeginImageContextWithOptions(watermarkView!.imageView.bounds.size, true, 0.0)
         watermarkView!.imageView.drawViewHierarchyInRect(watermarkView!.imageView.bounds, afterScreenUpdates: true)
         var img = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        UIImageWriteToSavedPhotosAlbum(img, self, "image:didFinishSavingWithError:contextInfo:", nil)
+        UIImageWriteToSavedPhotosAlbum(img, self, "image:didFinishSavingWithError:contextInfo:", nil)*/
     }
     
     func image(image: UIImage, didFinishSavingWithError error: NSError, contextInfo:UnsafePointer<Void>) {
