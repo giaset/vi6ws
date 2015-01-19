0b452c1b972c61f709d5f801231e2c0eb1339a0f
Gianni Settino <g@miltonandparc.com>
2015-01-18 23:36:42 -0500

    VI6WS website button

--- vi6ws/DrawerViewController.swift
+++ vi6ws/DrawerViewController.swift
@@ -24,9 +24,10 @@ class DrawerViewController: UIViewController {
     }
     
     func websiteButtonPressed() {
-        
+        let url = NSURL(string: "http://www.vi6ws.com")
+        UIApplication.sharedApplication().openURL(url!)
     }
-    
+
     func aboutButtonPressed() {
         let aboutText = "'VI6WS' is a photography filter application that allows users the option to point-and-shoot or import pictures from their Photo Library in order to tag them with watermarks dedicated to the arts and culture of the city of Toronto, with the added possibility of directly sharing them to social media platforms such as Instagram, Facebook, and Twitter. The \"6\" is a diminutive of the city's area codes recently made popular by the city's biggest international star, Drake."
         
